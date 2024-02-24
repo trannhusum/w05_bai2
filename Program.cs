f8f83a286b7a4d9259d9ea5d6f3af8c07fe0a996
trannhusum <147652638+trannhusum@users.noreply.github.com>
2024-02-24 18:41:49 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w05_bai2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 4, 12, 7, 8, 1, 6, 9 };
+            int index = MinValue(arr);
+            Console.WriteLine("The smallest element in the array is: " + arr[index]);
+            Console.ReadKey();
+        }
+        public static int MinValue(int[] array)
+        {
+
+            int min = array[0];
+            int index = 0;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                    index = i;
+                }
+            }
+
+            return index;
+        }
+    }
+}
